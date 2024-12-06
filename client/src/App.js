8d750049740b768ecb38c9cf707435edd1a0185d
InderParmar <iparmar1@myseneca.ca>
2024-12-06 12:18:18 -0500

    Initial commit for IT Support Dashboard

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+      <div className="text-center text-2xl font-bold text-blue-600">
+          Tailwind CSS is Working!
+      </div>
+  );
+};
+
+export default App;
